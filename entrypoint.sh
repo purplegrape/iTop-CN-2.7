efe470c455a982def98171e4376d5e73be781bc2
purplegrape <purplegrape4@gmail.com>
2023-07-04 02:31:08 +0800

    add dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+if [ ! -d /var/www/html ];then
+    rsync -aqu /usr/src/itop/ /var/www/html/
+    mkdir -p /var/www/html/{data,conf,log,env-production,env-production-build}
+    chown -R www-data.www-data /var/www/html/{data,conf,log,env-production,env-production-build}
+    echo -e "open_basedir=/var/www/html" > /var/www/html/.user.ini
+fi
+
+exec -c $@
