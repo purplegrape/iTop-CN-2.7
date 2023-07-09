2dc31cf1dabfbc5ee73f86108688d99c3655c49f
purplegrape <purplegrape4@gmail.com>
2023-07-10 04:03:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@ if [ ! -d /var/www/html ];then
     rsync -aqu /usr/src/web/ /var/www/html/
     mkdir -p /var/www/html/{data,conf,log,env-production,env-production-build}
     chown -R www-data.www-data /var/www/html/{data,conf,log,env-production,env-production-build}
-    echo -e "open_basedir=/var/www/html" > /var/www/html/.user.ini
 fi
 
 exec -c $@
